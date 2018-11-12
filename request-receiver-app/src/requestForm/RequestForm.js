5b02a9c267357b5b77e756fff36051b549dab471
KuaqSon <qson.ng@gmail.com>
2018-11-12 17:33:30 +0700

    Add submit request receiver

--- request-receiver-app/src/requestForm/RequestForm.js
+++ request-receiver-app/src/requestForm/RequestForm.js
@@ -1,39 +1,40 @@
 import React, { Component } from 'react';
 import './RequestForm.css';
 import { Col, Row, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
-// import Textfield from 'material-ui/Textfield';
-// import axios from 'axios';
+import axios from 'axios';
 
 class RequestForm extends Component {
-
-
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            phone: '',
+            address: '',
+            city: '',
+            state: '',
+            note: '',
+        };
         this.onSubmit = this.handleSubmit.bind(this);
-
     }
 
-    handleSubmit(e) {
+    handleSubmit = (e) => {
         e.preventDefault();
-        var self = this;
-        var data = new FormData();
-        // payload
-        // data.append("myjsonkey", JSON.stringify(payload));
+        var data = this.state;
+        // to do: hard code what field want to submit to server, not submit all state
 
-        fetch('http://localhost:3000/request/add', {
-            method: 'POST',
-            mode: 'CORS',
-            body: JSON.stringify(data),
-            headers: {
-                // 'Authorization': `bearer ${token}`,
-                'Content-Type': 'application/json'
-            }
+        console.log(data);
+
+        axios.post('http://localhost:3000/request/test', data)
+        .then(res => console.log(res));
+
+        this.setState({
+            name: '',
+            phone: '',
+            address: '',
+            city: '',
+            state: '',
+            note: '',
         })
-            .then(function (response) {
-                return response.json()
-            }).then(function (body) {
-                console.log(body);
-            });
     }
 
     // componentDidMount() {
@@ -65,71 +66,72 @@ class RequestForm extends Component {
 
         return (
 
-            <div className="request-form">Data have been loaded
-              <div className="header-request">
+            <div className="request-form">
+                <div className="header-request">
                     Doubble Son
                     <br></br>
                     Take car
-            </div>
-
-                {/* {items.map(item => (
-                    <li key={item.id}>
-                        => Name: {item.name}
-                        Telephone: {item.telephone}
-                        Address: {item.address}
-                        Infor: {item.infor}
-
-                    </li>
-
-                ))} */}
-                <Form className="form-container" onSubmit={self.onSubmit}>
-
+                </div>
+                <Form className="form-container" onSubmit={this.onSubmit}>
                     <Row form>
                         <Col md={6}>
-                            <FormGroup ref="name">
+                            <FormGroup>
                                 <Label for="exampleEmail">Name</Label>
-                                <Input type="text" name="name" id="exampleEmail" placeholder="with a placeholder" floatingLabelText="name"/>
+                                <Input type="text" name="name" id="exampleEmail" placeholder="with a placeholder"
+                                value={this.state.name}
+                                onChange={e => this.setState({ name: e.target.value })}
+                                />
                             </FormGroup>
                         </Col>
                         <Col md={6}>
-                            <FormGroup ref="telephone">
+                            <FormGroup>
                                 <Label for="examplePassword">Phone Number</Label>
-                                <Input type="tel" name="phone" id="examplePassword" placeholder="password placeholder" floatingLabelText="telephone" />
+                                <Input type="tel" name="phone" id="examplePassword" placeholder="password placeholder"
+                                value={this.state.phone}
+                                onChange={e => this.setState({ phone: e.target.value })}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
                     <FormGroup>
                         <Label for="exampleAddress">Address</Label>
-                        <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St" floatingLabelText="address"/>
+                        <Input type="text" name="address" id="exampleAddress" placeholder="1234 Main St"
+                        value={this.state.address}
+                        onChange={e => this.setState({ address: e.target.value })}
+                        />
                     </FormGroup>
                     <Row form>
                         <Col md={8}>
-                            <FormGroup ref="infor">
+                            <FormGroup>
                                 <Label for="exampleCity">Information</Label>
-                                <Input type="text" name="city" id="exampleCity" floatingLabelText="infor" />
+                                <Input type="text" name="city" id="exampleCity"
+                                value={this.state.city}
+                                onChange={e => this.setState({ city: e.target.value })}
+                                />
                             </FormGroup>
                         </Col>
                         <Col md={4}>
-                            <FormGroup ref="state">
+                            <FormGroup>
                                 <Label for="exampleState">State</Label>
-                                <Input type="text" name="state" id="exampleState"  floatingLabelText="state"/>
+                                <Input type="text" name="state" id="exampleState"
+                                value={this.state.state}
+                                onChange={e => this.setState({ state: e.target.value })}
+                                />
                             </FormGroup>
                         </Col>
                     </Row>
                     <FormGroup>
                         <Label for="exampleAddress2">Notes</Label>
-                        <Input type="textarea" name="note" id="exampleNote" placeholder="Apartment, studio, or floor" />
+                        <Input type="textarea" name="note" id="exampleNote" placeholder="Apartment, studio, or floor"
+                        value={this.state.note}
+                        onChange={e => this.setState({ note: e.target.value })}
+                        />
                     </FormGroup>
                     <Button>Book</Button>
                 </Form>
-
-
-
-
-            </div>)
-        // });
+            </div>
+        )
     }
-    // }
 }
 
 
--- server/app.js
+++ server/app.js
@@ -33,7 +33,7 @@ var corsOptions = {
 
 var app = express();
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(bodyParser.urlencoded({
     extended: false
